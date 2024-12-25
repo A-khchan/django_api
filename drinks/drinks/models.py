2b22dd8411368d2fc1cc44da839aede876aeb281
Kong Hop Chan <albert88hk@yahoo.com.hk>
2024-12-24 16:52:46 -0800

    commit

--- drinks/drinks/models.py
+++ drinks/drinks/models.py
@@ -68,3 +68,10 @@ class DeliveredItems(models.Model):
     item = models.CharField(max_length=20)
     quantity1 = models.IntegerField()
     quantity2 = models.IntegerField()
+
+class ItemSetup(models.Model):
+    itemCode = models.CharField(max_length=20)
+    itemDesc = models.CharField(max_length=100)
+    quantity2Flag = models.CharField(max_length=1)
+    bags = models.IntegerField(default=1)
+    bagPrice = models.FloatField(default=0.0)
